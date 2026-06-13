e60f12f9879ab7c355ce34348192acbc52935c4a
tarnaola001-stack <tarnaola001@gmail.com>
2026-06-14 02:28:34 +0400

    configure client for amvera backend

--- client/src/utils/axiosFetch.js
+++ client/src/utils/axiosFetch.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosFetch = axios.create({
     // Принудительно указываем адрес нашего Node.js сервера 
-    baseURL: "http://localhost:8080/api", 
+    baseURL: "https://freelancepf-api-tarnaola001.amvera.io/api", 
     withCredentials: true
 });
 
--- client/src/utils/generateImageURL.js
+++ client/src/utils/generateImageURL.js
@@ -4,16 +4,18 @@ const generateImageURL = async (image) => {
   const file = new FormData();
   file.append("file", image);
 
-  // Синхронизировано с портом 8080 вашего server.js из технического отчета
-  const backendUrl = import.meta.env.VITE_API_URL || "http://localhost:8080";
-
   const { data } = await axios.post(
-    `${backendUrl}/api/gigs/upload-local`, 
+    "https://freelancepf-api-tarnaola001.amvera.io/api/gigs/upload-local",
     file,
-    { headers: { "Content-Type": "multipart/form-data" } }
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      withCredentials: true,
+    }
   );
-  
-  return { url: data.url }; 
+
+  return { url: data.url };
 };
 
 export default generateImageURL;
