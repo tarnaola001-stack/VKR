ee0420b42ab8be967a7a2eca132a989f52666f31
tarnaola001-stack <tarnaola001@gmail.com>
2026-06-14 03:28:54 +0400

    allow vercel client origin

--- server/src/server.js
+++ server/src/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(compression());
 app.use(cors({
-origin: ['http://localhost:5173', 'http://localhost:4173', 'https://netlify.app'],
+origin: ['http://localhost:5173', 'http://localhost:4173', 'https://vkr-kohl.vercel.app'],
 credentials: true
 }));
 
